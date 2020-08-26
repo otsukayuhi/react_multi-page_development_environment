27af3bf90a18995ea6e35fdf4ae05093fcbcfff3
Otsuka Yuhi <11628469+otsukayuhi@users.noreply.github.com>
2020-08-27 01:45:29 +0900

    makeMediaQuery を修正

--- src/util/makeMediaQuery.ts
+++ src/util/makeMediaQuery.ts
@@ -7,20 +7,16 @@ type MakeMediaQuery = {
 /**
  * メディアクエリ用のオブジェクトを生成
  * @example
- * import { makeMediaQuery } from 'util/getMediaQuery';
+ * import { makeMediaQuery } from 'util/makeMediaQuery';
  * const mediaQuery = makeMediaQuery();
  * console.log(mediaQuery.md);
  * // [at]media screen and (min-width: 768px)
  */
 export const makeMediaQuery: () => MakeMediaQuery = () => {
-  let mediaQuery: MakeMediaQuery;
+  const mediaQueries = Object.entries(breakPoints).map((breakPoint) => [
+    breakPoint[0],
+    `@media screen and (min-width: ${breakPoint[1]}px)`,
+  ]);
 
-  for (const [key, value] of Object.entries(breakPoints)) {
-    mediaQuery = {
-      ...mediaQuery,
-      [key]: `@media screen and (min-width: ${value}px)`,
-    };
-  }
-
-  return mediaQuery;
+  return Object.fromEntries(mediaQueries);
 };
